a669514c47db77de95d938bc7af092be02655ff0
Nilesh <nilesh.tudu@nirmitee.io>
2023-07-05 20:18:30 +0530

    First build complete

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import HomeScreen from './src/HomeScreen';
+import SettingsScreen from './src/SettingsScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
+import { TourGuideProvider } from './srcs';
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+    //eas update --branch dev --message "Updating the app"
+  //eas update --branch prod --message "Updating the app"
+  //eas build -p android --profile prod
+  //  eas build --platform android
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+      <TourGuideProvider {...{ borderRadius: 16, androidStatusBarVisible: true }}>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </TourGuideProvider>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
